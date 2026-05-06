ffc4cafb3e87325dd9f6c23da3921198a686337b
xiaochai777 <104331324+xiaochai777@users.noreply.github.com>
2026-05-06 18:40:59 +0800

    fix: 恢复中文主题和管理后台

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '柴柴的博客';
+export const SITE_DESCRIPTION = '记录生活，分享思考，探索世界';
